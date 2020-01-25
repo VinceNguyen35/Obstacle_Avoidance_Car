305faec94d256cc6a210a8a06ed6257676ee0265
Vinnydaninja <44877059+Vinnydaninja@users.noreply.github.com>
2020-01-25 02:24:21 -0800

    Update ADCSWTrigger.c

--- ADCSWTrigger.c
+++ ADCSWTrigger.c
@@ -1,28 +1,3 @@
-// ADCSWTrigger.c
-// Runs on LM4F120/TM4C123
-// Provide functions that sample ADC inputs PE1, PE4, PE5
-// using SS2 to be triggered by software and trigger two
-// conversions,wait for them to finish, and return the two
-// results.
-// Daniel Valvano
-// September 12, 2013
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
-
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
-
 #include "ADCSWTrigger.h"
 #include "lm4f120h5qr.h"
 
