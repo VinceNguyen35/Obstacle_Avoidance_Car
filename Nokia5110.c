8f61dab1f64d54201fc27740f092833f8e26cc1a
Vinnydaninja <44877059+Vinnydaninja@users.noreply.github.com>
2020-01-25 02:23:11 -0800

    Update Nokia5110.c

--- Nokia5110.c
+++ Nokia5110.c
@@ -1,33 +1,3 @@
-// Nokia5110.c
-// Runs on LM4F120/TM4C123
-// Use SSI0 to send an 8-bit code to the Nokia5110 48x84
-// pixel LCD to display text, images, or other information.
-// Daniel Valvano
-// September 16, 2013
-
-// Font table, initialization, and other functions based
-// off of Nokia_5110_Example from Spark Fun:
-// 7-17-2011
-// Spark Fun Electronics 2011
-// Nathan Seidle
-// http://dlnmh9ip6v2uc.cloudfront.net/datasheets/LCD/Monochrome/Nokia_5110_Example.pde
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
-
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
-
 // Blue Nokia 5110
 // ---------------
 // Signal        (Nokia 5110) LaunchPad pin
