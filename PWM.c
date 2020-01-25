4395a0a3ee853926f3c23247b3f505591dcb8fb8
Vinnydaninja <44877059+Vinnydaninja@users.noreply.github.com>
2020-01-25 02:21:55 -0800

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -1,24 +1,3 @@
-// PWM.c
-// Runs on TM4C123
-// Use PWM0/PB6 and PWM1/PB7 to generate pulse-width modulated outputs.
-// Daniel Valvano
-// March 28, 2014
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2014
-  Program 6.7, section 6.3.2
- Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
 
